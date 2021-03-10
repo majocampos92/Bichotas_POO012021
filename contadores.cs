505de08bbc0b3810475b8d129cc7f75ecbcfa965
majocampos92 <maria_campos84@hotmail.com>
2021-03-10 11:15:15 -0600

    change

--- contadores.cs
+++ contadores.cs
@@ -1,28 +1,28 @@
 using System;
 
-namespace piramide
+namespace pyramid
 {    
     internal class Program {
 
         public static void Main(string[] args){
             
-            int numero = 0;
+            int number = 0;
             
-            Console.WriteLine("Ingrese el numero: ");
+            Console.WriteLine("Enter a number: ");
 	        numero = Int32.Parse(Console.ReadLine());
 
             AddNumbers(numero);
         }
-        public static int AddNumbers(int numero)
+        public static int AddNumbers(int number)
         {
             int i = 0, j = 0;
-            for( i=1; i<=numero; i++){
-                for(j=numero; j>i; j--){
+            for( i=1; i<=number; i++){
+                for(j=number; j>i; j--){
                     Console.Write($"{j}");
                 }
                     Console.WriteLine($"");
             }
-            return numero;
+            return number;
         }
     }
 }
