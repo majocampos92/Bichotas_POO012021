e1c75ae63919364bde7e577248409128b1d85397
federico-ramirez <00009220@uca.edu.sv>
2021-03-10 20:32:33 -0600

    MultiplyNumbers

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -14,11 +14,11 @@ namespace Example1
             Console.WriteLine("Enter a value for b:");
             b = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)}");
+            Console.WriteLine($"Multiplying {a} * {b} equals {MultiplyNumbers(a, b)}");
         }
-        public static int AddNumbers(int a, int b)
+        public static int MultiplyNumbers(int a, int b)
         {
-            return a-b;
+            return a*b;
         }
     }
 }
